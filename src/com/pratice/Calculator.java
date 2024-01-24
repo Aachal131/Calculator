5b614a9e456908696f870f557d921ec04f0b2dfe
aachal <aachalkborole131@gmail.com>
2024-01-24 21:40:07 +0530

    first commit

---
+++ src/com/pratice/Calculator.java
@@ -0,0 +1,12 @@
+package com.pratice;
+
+public class Calculator {
+    public static void main(String[]args){
+        System.out.println("Welcome to Git");
+        int a =10,b=20,c;
+        c=a+b;
+        System.out.println("Addition is c"+c);
+        c=a-b;
+        System.out.println("Subtraction is c"+c);
+    }
+}
